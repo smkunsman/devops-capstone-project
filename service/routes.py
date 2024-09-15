50182204d5798244d1bdddd2497f516a094de942
smkunsman <selenakunsman@gmail.com>
2024-09-14 22:36:26 -0400

    added list  route and tested

--- service/routes.py
+++ service/routes.py
@@ -81,6 +81,22 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
